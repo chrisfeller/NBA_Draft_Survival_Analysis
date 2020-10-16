00fd2fbd256c92c841f312a1f3182d48fbcbb9f3
Chris Feller <chrisfeller10@gmail.com>
2020-10-16 12:50:16 -0600

    first commit

---
+++ readme.md
@@ -0,0 +1,90 @@
+<center> <h1>NBA Draft Survival Analysis</h1> </center>
+<center> <h3>Updated October 16, 2020</h3> </center>
+
+---
+#### Motivation
+In preparation for the 2020 NBA Draft, we wanted to analyze when each prospect was likely to be selected. Compiling 20 mock drafts from various draft analysts, we created a survival analysis which calculated the probability of each prospect being available at each draft slot.
+
+As an example, the following plot illustrates the probability that Anthony Edwards is available at each draft slot. Moving from right to left on the x-axis, we observe that based on the compiled mock drafts there is ~45% probability Edwards is available at pick number 2 and ~30% probability he is available at pick number 3.
+
+<div style="text-align:center"><img src='plots/players/Anthony_Edwards.png' width=70%></div>
+
+<br>
+
+Survival analysis is typically used to calculate the probability of death occurring at a given age for subsets of the population. In this case, I'm substituting being drafted for death and draft slot for age. The time between the start of the analysis and when the event of interest occurs is called survival time, which in this case is the number of draft slots that a player is still available in each mock draft.
+
+The analysis calculates probabilities for picks 1-60. Not all players were included in each mock draft and not every mock draft ranked players 1-60 with most only ranking through the first round. Since those players were never observed we've considered them right-censored. This means that we have some information about their expected selection, in most cases they were not selected picks 1-30, but not the exact slot in which the analyst had them mocked. Due to this constraint, we've used the Kaplan-Meier estimator to calculate the survival curve of each prospect.
+
+
+#### Mock Drafts
+
+Each player's survival curve is based on information compiled from 20 mock drafts listed below. We've not included the underlying data in this repo as two mocks are behind a paywall, but have stated when each was last updated. We'll check for updates and re-run the analysis on a weekly basis in the run-up to the draft. If you see any updates we've missed or mocks we should add let us know! Thanks to all of these individuals who publish their mock drafts, especially to those who do it in tabular form.
+
+| Name  | Affiliation  | Updated  | Mock Draft  |
+|---|---|---|---|
+| Babcock Hoops  | Babcock Hoops  | October 5  | [Link](https://www.babcockhoops.com/mockdraft)  |
+| Bryan Kalbrosky  | Rookie Wire  | August 20  | [Link](https://therookiewire.usatoday.com/lists/2020-nba-mock-draft-all-60-picks-post-lottery/)  |
+| Chris Stone  | Sporting News  | March 24  | [Link](https://www.sportingnews.com/us/nba/news/nba-mock-draft-2020-warriors-pick-timberwolves-lamelo-ball/178cr8zivu85e1qdtavgxp9cc2)  |
+| Danny Cunningham  | Complex  | October 12  | [Link](https://www.complex.com/sports/nba-mock-draft-predictions-post-nba-finals/?utm_campaign=sportstw&utm_source=twitter.com&utm_medium=social)  |
+| Draft Express  | ESPN  | August 20  | [Link](https://www.espn.com/nba/insider/story/_/id/29695731/nba-mock-draft-projected-picks-big-lottery-winners)  |
+| Gary Parrish  | CBS Sports  | October 9  | [Link](https://www.cbssports.com/nba/draft/mock-draft/)  |
+| James Ham  | NBC Sports | October 12  | [Link](https://www.nbcsports.com/bayarea/warriors/2020-nba-mock-draft-180-first-round-pick-projections-after-lakers-win-finals)  |
+| Jeff Goodman  | Stadium  | August 21  | [Link](https://watchstadium.com/jeff-goodmans-2020-nba-mock-draft-3-0-lottery-order-revealed-08-20-2020/)  |
+| Jeremy Woo  | Sports Illustrated  | October 14  | [Link](https://www.si.com/nba/2020/10/14/nba-mock-draft-projections-post-finals)  |
+| Jonathan Wasserman  | Bleacher Report  | October 6  | [Link](https://bleacherreport.com/articles/2912058-2020-nba-mock-draft-simulating-3-trades-in-historically-unpredictable-lottery)  |
+| Kevin O'Connor  | The Ringer  | October 14  | [Link](https://nbadraft.theringer.com/)  |
+| Kevin Pelton  | ESPN  | June 24  | [Link](https://www.espn.com/nba/insider/story/_/id/29335461/2020-nba-draft-projections-ranking-top-30-prospects)  |
+| Krysten Peek  | Yahoo Sports  | October 15  | [Link](https://sports.yahoo.com/2020-nba-mock-draft-50-la-melo-moves-to-no-1-132728170.html)  |
+| Kyle Boone  | CBS Sports  | October 15  | [Link](https://www.cbssports.com/nba/draft/mock-draft/)  |
+| NBAdraftnet | NBAdraftnet | September 16 | [Link](https://www.nbadraft.net/nba-mock-drafts/) |
+| NetScouts  | NetScouts  | October 5  | [Link](https://netscoutsbasketball.com/scouting/2020-nba-mock-draft/)  |
+| Ricky O'Donnell  | SB Nation  | August 20  | [Link](https://www.sbnation.com/nba/2020/8/20/21374098/nba-mock-draft-2020-lottery-order-timberwolves-warriors-bulls)  |
+| Sam Veceine  | The Athletic  | October 13  | [Link](https://theathletic.com/2128239/2020/10/13/lamelo-ball-over-james-wiseman-at-no-1-in-2020-nba-mock-draft-version-8-0/)  |
+| Scott Gleeson  | USA Today  | August 20  | [Link](https://www.usatoday.com/story/sports/nba/draft/2020/08/20/nba-mock-draft-post-lottery-timberwolves-top-pick/3402893001/)  |
+| Tankathon  | Tankathon  | ~ August  | [Link](http://www.tankathon.com/mock_draft)  |
+
+#### Consensus Top-3 Picks
+
+Based on the average draft slot across all mock drafts, Anthony Edwards (1.8), LaMelo Ball (2.2), and James Wiseman (3.5) are the consensus top three picks. Their respective survival curves are plotted below. At this point in time only Ball and Edwards are expected to be selected in the top two picks. Ball and Wiseman have low probabilities of lasting to picks 9 and 17, respectively, as a few mocks have them going quite low.
+
+<div style="text-align:center"><img src='plots/top_3.png' width=90%></div>
+
+#### Consensus Top-10 Picks
+
+In similar fashion, here are the survival curves of the consensus top-10 picks. Six of the top ten have a non-zero chance of falling out of the lottery according to the mock drafts. One thing we have our eye on is any future movement in Devin Vassell's curve after the suspected [change to his shooting form](https://twitter.com/Joe_Casey1/status/1316803378182262784?s=20) that have [some analysts questining their opinion of him](https://twitter.com/johnhollinger/status/1316794770220556288?s=20).
+
+<div style="text-align:center"><img src='plots/top_10.png' width=90%></div>
+
+#### Case Studies
+
+At this point the mock drafts have Anthony Edwards as the most likely first overall pick. There's ~45% probability he is available at the number two pick and ~30% chance he is available at the third pick. Based on the mocks there is no chance he falls to the 5th pick.
+
+<div style="text-align:center"><img src='plots/players/Anthony_Edwards.png' width=90%></div>
+
+<br>
+
+Xavier Tillman provides a good example of a player likely to go in the middle of the draft. Based on the mock drafts, he is unlikely to be selected prior to the 23rd slot. After then his probability of being available decreases at a steady rate. He is likely to be gone by pick 52.
+
+<div style="text-align:center"><img src='plots/players/Xavier_Tillman.png' width=90%></div>
+
+<br>
+
+Kenyon Martin Jr. is a player on the fringe of being drafted. The mock drafts have him unlikely to be selected prior to pick number 56. In total, there is ~60% probability he goes undrafted.
+
+<div style="text-align:center"><img src='plots/players/Kenyon_Martin_Jr.png' width=90%></div>
+
+<br>
+
+Hopefully this analysis provides additional information for teams on when targeted players might be available during the draft. As a final example, if you're a hypothetical team targeting a point guard outside of the lottery you can compare survival curves to better predict when a player will be available. In this case, if you value Maledon over Flynn and Terry you'll need to be more aggressive in moving up to get him as he's likely to be gone before the other two.
+
+<br>
+
+<div style="text-align:center"><img src='plots/pg_example.png' width=90%></div>
+
+#### Next Steps
+We're working on a dashboard to provide users the ability to interactively filter to player(s).
+
+#### References
+1. [Lifelines Package Documentation](https://lifelines.readthedocs.io/en/latest/index.html)
+2. Emily Zabor - *[Survival Analysis in R](https://www.emilyzabor.com/tutorials/survival_analysis_in_r_tutorial.html#part_1:_introduction_to_survival_analysis)*
+3. Savvas Tjortjoglou - *[Surviving the NFL](http://savvastjortjoglou.com/nfl-survival-analysis-kaplan-meier.html)*
---
+++ scripts/survival_analysis.py
@@ -0,0 +1,333 @@
+import pandas as pd
+import numpy as np
+from functools import reduce
+import difflib
+from lifelines import KaplanMeierFitter
+import matplotlib.pyplot as plt
+
+plt.style.use('fivethirtyeight')
+
+def fuzzy_name_match(x):
+    """
+    Fuzzy matches a given player's names from an individual mock draft with
+    the player's 'true' name in full prospect list.
+
+    Args:
+        x (str): an individual player's name
+
+    Returns:
+        x (str): fuzzy match of input player's name from full prospect list
+    """
+    # Read in prospect names
+    prospect_df = pd.read_csv('../data/prospects.csv')
+    # try to obtain fuzzy match for input player name
+    # exception returns none if there is no match
+    try:
+        return difflib.get_close_matches(x, prospect_df['player'])[0]
+    except:
+        return None
+
+def pre_process_mocks(df):
+    """
+    Updates each player name in a given mock draft with the fuzzy match name
+    from the full prospect list. Allows all mock drafts to be easily joined
+    together as not all mock's follow the same naming conventions. For example,
+    some mocks have Xavier Tillman Sr. and others have Xavier Tillman.
+
+    Args:
+        df (pandas DataFrame): an individual mock draft
+
+    Returns:
+        df (pandas DataFrame): an individual mock draft with player names
+                               updated to fuzzy match name from full prospect
+                               list
+    """
+    # create fuzzy match name for each player, which will be used as join
+    # column when merging with other mock drafts
+    df['fuzzy_name'] = df['player'].apply(lambda x: fuzzy_name_match(x))
+    # drop original player name
+    df = df.drop('player', axis=1)
+    return df
+
+def plot_player(player, melt_df, save=False):
+    """
+    Plots the survival curve for an individual player either displaying the
+    output or saving to the `../plots/players` directory.
+
+    Args:
+        player (str): The name of an individual player from the full
+                      prospect list
+        melt_df (pandas DataFrame): long-form big board dataframe with duration
+                                    and censor columns
+        save (boolean): Boolean of whether to write out the .png file in the
+                        `../plots/players` director or display the image
+
+    Returns:
+        None
+    """
+    # instantiate Kaplan-Meier survival model
+    kmf = KaplanMeierFitter()
+    # create matplotlib figure
+    fig, ax = plt.subplots(figsize=(18, 8))
+    # slice to individual player
+    idx = melt_df.player == player
+    # get the maximum draft slot for given player for x-axis plotting purposes
+    x_max = min(60, melt_df.query("player=='{0}'".format(player))['duration'].max())
+    # fit Kaplan-Meier survival model
+    kmf.fit(melt_df.duration[idx], melt_df.observed[idx])
+    # plot survival curve
+    kmf.plot(ax=ax, legend=False)
+    # set axes, titles. etc.
+    ax.set(title='Survival Curve for {0}'.format(player),
+           xlabel='Draft Slot',
+           ylabel='Probability {0} is Still Available'.format(player.split()[-1]),
+           xlim=(0,x_max+0.1),
+           ylim=(-0.1,1.1))
+    y_vals = ax.get_yticks()
+    # format tickmarks
+    ax.set_yticklabels(['{:3.0f}%'.format(x * 100) for x in y_vals])
+    plt.xticks(range(0, int(x_max)+1), fontsize=10)
+    ax.set_xticklabels(['{0}'.format(int(x)) for x in range(1, int(min(61, x_max+2)))])
+    # either save figure or display
+    if save:
+        plt.savefig('../plots/players/{0}'.format(player.lower().replace(' ', '_')))
+    else:
+        plt.show()
+
+def plot_Consensus_top_10(big_board_df, melt_df, save=False):
+    """
+    Plots the survival curve for the Consensus top-10 players, either displaying
+    the output or saving to the `../plots` directory.
+
+    Args:
+        big_board_df (pandas DataFrame): the wide-form big board dataframe with
+                                        player names and draft slots
+        melt_df (pandas DataFrame): long-form big board dataframe with duration
+                                    and censor columns
+        save (boolean): Boolean of whether to write out the .png file in the
+                        `../plots` director or display the image
+
+    Returns:
+        None
+    """
+    # consensus Top-10 Picks (By Average Ranking)
+    top_10 = big_board_df['player'].to_list()[0:10]
+    # create matplotlib figure with 10 subplots
+    fig, axs = plt.subplots(nrows=10, ncols=1, sharey=True, sharex=False, figsize=(15,32))
+    # loop through top 10 players plotting each to their respective subplot
+    for player, ax in zip(top_10, axs.flatten()):
+        # slice to individual player
+        idx = melt_df.player == player
+        # fit Kaplan-Meier survival model
+        kmf = KaplanMeierFitter()
+        kmf.fit(melt_df.duration[idx], melt_df.observed[idx])
+        # plot individual player's survival curve
+        kmf.plot(ax=ax, legend=False)
+        # format xticks, etc.
+        ax.set(title=player, xlabel='', xlim=(0,14), ylim=(-0.1, 1.1))
+        y_vals = ax.get_yticks()
+        ax.set_yticklabels(['{:3.0f}%'.format(x * 100) for x in y_vals])
+        ax.set_xticks(range(0, 15))
+        ax.set_xticklabels(['{0}'.format(int(x)) for x in range(1, 15)])
+    # set title, axes, etc.
+    fig.text(0.5, 0.001, "Draft Slot", ha="center", fontsize=18)
+    fig.text(0.001, 0.5, "Probability Player is Still Available",
+             va="center", rotation="vertical", fontsize=18)
+    fig.suptitle("Survival Curve for Consensus Top-10 Picks",
+                 fontsize=35)
+    fig.tight_layout()
+    fig.subplots_adjust(top=0.95)
+    # either save figure or display
+    if save:
+        plt.savefig('../plots/top_10.png')
+    else:
+        plt.show()
+
+def plot_Consensus_top_3(big_board_df, melt_df, save=False):
+    """
+    Plots the survival curve for the Consensus top-3 players, either displaying
+    the output or saving to the `../plots` directory.
+
+    Args:
+        big_board_df (pandas DataFrame): the wide-form big board dataframe with
+                                        player names and draft slots
+        melt_df (pandas DataFrame): long-form big board dataframe with duration
+                                    and censor columns
+        save (boolean): Boolean of whether to write out the .png file in the
+                        `../plots` director or display the image
+
+    Returns:
+        None
+    """
+    # consensus Top-3 Picks (By Average Ranking)
+    top_3 = big_board_df['player'].to_list()[0:3]
+    # get the maximum draft slot for given players for x-axis plotting purposes
+    max_player = melt_df.query("player == @top_3")['duration'].max()
+    # list of line styles for each player
+    line_styles = ['-', '--', ':', '-.']
+    # create matplotlib figure
+    fig, ax = plt.subplots(figsize=(18, 8))
+    # loop through top 3 players
+    for player, ls in zip(top_3, line_styles):
+        # slice to individual player
+        idx = melt_df.player == player
+        # fit Kaplan-Meier survival mode
+        kmf = KaplanMeierFitter()
+        kmf.fit(melt_df.duration[idx], melt_df.observed[idx])
+        # plot player's survival curve
+        kmf.plot(ax=ax, ci_show=False, label=player, ls=ls)
+        # format title, axes, ticks
+        ax.set(title='Survival Curve for Consensus Top-3 Picks',
+               xlabel='Draft Slot',
+               ylabel='Probability Player is Still Available',
+               xlim=(0,max_player+1), ylim=(-0.1, 1.1))
+        y_vals = ax.get_yticks()
+        ax.set_yticklabels(['{:3.0f}%'.format(x * 100) for x in y_vals])
+        plt.xticks(range(0, max_player+2))
+        ax.set_xticklabels(['{0}'.format(int(x)) for x in range(1,max_player+2)])
+    fig.tight_layout()
+    fig.subplots_adjust(top=0.95)
+    # either save figure or display
+    if save:
+        plt.savefig('../plots/top_3.png')
+    else:
+        plt.show()
+
+def plot_multiple_players(player_list, melt_df):
+    """
+    Plots the survival curve for input player(s)
+
+    Args:
+        player_list (list): list of player names to plot survival curves
+        melt_df (pandas DataFrame): long-form big board dataframe with duration
+                                    and censor columns
+
+    Returns:
+        None
+    """
+    # create matplotlib figure
+    fig, ax = plt.subplots(figsize=(18, 8))
+    # list of line styles for each player
+    line_styles = ['-', '--', ':', '-.']
+    # get the maximum draft slot for given players for x-axis plotting purposes
+    max_player = melt_df.query("player == @player_list")['duration'].max()
+    # loop through players
+    for player, ls in zip(player_list, line_styles):
+        # slice to individual player
+        idx = melt_df.player == player
+        # fit Kaplan-Meier survival mode
+        kmf = KaplanMeierFitter()
+        kmf.fit(melt_df.duration[idx], melt_df.observed[idx])
+        # plot player's survival curve
+        kmf.plot(ax=ax, ci_show=False, label=player, ls=ls)
+        # format title, axes, ticks
+        ax.set(title='Survival Curve',
+               xlabel='Draft Slot',
+               ylabel='Probability Player is Still Available',
+               xlim=(0, min(60, int(max_player))), ylim=(-0.1, 1.1))
+        y_vals = ax.get_yticks()
+        ax.set_yticklabels(['{:3.0f}%'.format(x * 100) for x in y_vals])
+        plt.xticks(range(0, min(61, int(max_player)+2)), fontsize=10)
+        ax.set_xticklabels(['{0}'.format(int(x)) for x in range(1, min(61, int(max_player)+2))])
+    fig.tight_layout()
+    fig.subplots_adjust(top=0.95)
+    plt.show()
+
+if __name__=='__main__':
+    # Read in individual mock drafts
+    babcock_hoops_df = pd.read_csv('../data/babcock_hoops.csv')
+    bryan_kalbrosky_df = pd.read_csv('../data/bryan_kalbrosky.csv')
+    chris_stone_df = pd.read_csv('../data/chris_stone.csv')
+    danny_cunningham_df = pd.read_csv('../data/danny_cunningham.csv')
+    draft_express_df = pd.read_csv('../data/draft_express.csv')
+    gary_parrish_df = pd.read_csv('../data/gary_parrish.csv')
+    james_ham_df = pd.read_csv('../data/james_ham.csv')
+    jeff_goodman_df = pd.read_csv('../data/jeff_goodman.csv')
+    jeremy_woo_df = pd.read_csv('../data/jeremy_woo.csv')
+    jonathan_wasserman_df = pd.read_csv('../data/jonathan_wasserman.csv')
+    kevin_oconnor_df = pd.read_csv('../data/kevin_oconnor.csv')
+    kevin_pelton_df = pd.read_csv('../data/kevin_pelton.csv')
+    krysten_peek_df = pd.read_csv('../data/krysten_peek.csv')
+    kyle_boone_df = pd.read_csv('../data/kyle_boone.csv')
+    nbadraftnet_df = pd.read_csv('../data/nbadraftnet.csv')
+    netscouts_df = pd.read_csv('../data/netscouts.csv')
+    ricky_odonnell_df = pd.read_csv('../data/ricky_odonnell.csv')
+    sam_veceine_df = pd.read_csv('../data/sam_veceine.csv')
+    scott_gleeson_df = pd.read_csv('../data/scott_gleeson.csv')
+    tankathon_df = pd.read_csv('../data/tankathon.csv')
+
+    # Pre-process individual mock drafts to match names
+    mock_draft_list = [babcock_hoops_df, bryan_kalbrosky_df, chris_stone_df,
+                       danny_cunningham_df, draft_express_df, gary_parrish_df,
+                       james_ham_df, jeff_goodman_df, jeremy_woo_df,
+                       jonathan_wasserman_df, kevin_oconnor_df, kevin_pelton_df,
+                       krysten_peek_df, kyle_boone_df, nbadraftnet_df,
+                       netscouts_df, ricky_odonnell_df, sam_veceine_df,
+                       scott_gleeson_df, tankathon_df]
+
+    mock_draft_list = [pre_process_mocks(df) for df in mock_draft_list]
+
+    # Join individual mock drafts using fuzzy name matching
+    # Create an average rank for each player
+    big_board_df = reduce(lambda x, y: pd.merge(x, y,
+                                                on='fuzzy_name',
+                                                how='outer'),
+                                                mock_draft_list)
+    big_board_df = (big_board_df.rename({'fuzzy_name': 'player'},
+                                        axis=1)
+                                .assign(avg_rank=lambda x:
+                                                x[[col for col in big_board_df.columns
+                                                    if col != 'fuzzy_name']].mean(axis=1))
+                                .sort_values(by='avg_rank')
+                                .query("player.notnull()",
+                                        engine='python'))
+    big_board_df = big_board_df[['player'] + [ col for col in big_board_df.columns if col != 'player' ]]
+
+    # Write out joined draft boards
+    big_board_df.to_csv('../data/big_board.csv',
+                        index=False)
+
+    # Fill nulls with max draft slot +1 for each respective mock draft
+    # This indicates the next possible draft slot a player might have gone
+    # and will be censored as zero in the survival model
+    rank_cols =[col for col in big_board_df.columns if col not in ['player', 'avg_rank']]
+    big_board_df[rank_cols] = big_board_df[rank_cols].fillna(big_board_df[rank_cols].max() + 1,
+                                                             downcast='infer')
+
+    # Create observed flag to deal with censoring as not all mocks have ranked
+    # up to draft slot 60
+    mock_draft_name_list = [x.replace('_rank', '') for x in rank_cols if x != 'avg_rank']
+    observed_col_names = ['{0}_observed'.format(mock_draft) for mock_draft in mock_draft_name_list]
+    rank_col_names = ['{0}_rank'.format(x) for x in mock_draft_name_list]
+    big_board_df[observed_col_names] = big_board_df[rank_col_names].apply(lambda x: np.where(x<=x.max()-1, 1, 0))
+    observed_cols = [col for col in big_board_df.columns if '_observed' in col]
+
+    # Transform wide big board into long form
+    melt_1 = (pd.melt(big_board_df,
+                        id_vars='player',
+                        value_vars=rank_cols,
+                        value_name='duration')
+                 .assign(variable=lambda x: x['variable'].str.replace('_rank', '')))
+
+    melt_2 = (pd.melt(big_board_df,
+                        id_vars='player',
+                        value_vars=observed_cols,
+                        value_name='observed')
+                  .assign(variable=lambda x: x['variable'].str.replace('_observed', '')))
+
+    # Join duration and observed columns to create input dataframe for survival model
+    melt_df = (melt_1.merge(melt_2,
+                            on=['player', 'variable'],
+                            how='inner'))
+
+    # Plot an individual player's survival curve
+    plot_player('Anthony Edwards', melt_df, save=True)
+
+    # Plot survival curves of consensus top ten picks
+    plot_Consensus_top_10(big_board_df, melt_df, save=True)
+
+    # Plot survival curves of Consensus top ten picks
+    plot_Consensus_top_3(big_board_df, melt_df, save=True)
+
+    # Plot survival curves of multiple players
+    plot_multiple_players(['Theo Maledon', 'Malachi Flynn', 'Tyrell Terry'], melt_df)
